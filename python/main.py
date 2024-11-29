2a6a89286f954199fdd0a635d59cbeb6948af284
rune <jamesbedford@prontonmail.com>
2024-11-30 00:40:41 +1100

    Update to key generation and added encryption and decryption

--- python/main.py
+++ python/main.py
@@ -1,3 +1,5 @@
+import math
+
 class Key:
     def __init__(self, exponent, modulus):
         self.exponent = exponent
@@ -5,44 +7,47 @@ class Key:
 
 
 def create_keys(p: int, q: int):
-    n = p * q # modulus
-    r = (p - 1) * (q - 1) # Eulers totient theoraum provides the number of coprimes of n smaller than n.
-    e = max([x for x in range(1, r + 1) if coprime(x, n) and coprime(x, r)]) # Largest number between 1 and r that is coprime with both n and r.
-    d = pow(e, -1, r) # Lowest multiple of e that has a remainder of 1 when divided by r.
+    n = p * q
+    r = (p - 1) * (q - 1) # Eulers totient theorum provides the number of coprimes of n smaller than n.
+    e = find_e(n, r) # A number between 2 and r that is coprime with both n and r. This can often be defaulted to 3, 5 or commonly 65537 for large primes.
+    d = pow(e, -1, r) # Multiple of e that has a remainder of 1 when divided by r. In this case we will go with the first multiple that satisfies these conditions.
 
-    if d == e: # keys won't be secure if d and e are the same (happens if p, q are 3, 7). To resolve this add any multiple of r to d since d will always be one substracted from a multiple of r.
-        d += r
+    if d == e: # keys won't be secure if d and e are the same (happens if p, q are 3, 7).
+        d += r # To resolve this add any multiple of r to d since d will always be one substracted from a multiple of r.
 
     public_key = Key(e, n)
     private_key = Key(d, n)
 
     return (public_key, private_key)
 
-
-def find_greates_common_denominator(a, b):
-    # Everything divides 0 
-    if (a == 0 or b == 0): return 0
-    
-    # base case
-    if (a == b): return a
-    
-    if (a > b): 
-        return find_greates_common_denominator(a - b, b)
-            
-    return find_greates_common_denominator(a, b - a)
-
  
-def coprime(a, b):    
-    if ( find_greates_common_denominator(a, b) == 1): # if the gcd of two numbers is 1 then they are coprime
+def coprime(a, b):  
+    if math.gcd(a, b) == 1:
         return True
 
     return False 
 
+def find_e(n: int, r: int):
+    for i in range(2, r + 1):
+        if coprime(i, n) and coprime(i, r):
+            return i # In this case just finding the first/smallest number that is coprime to n and r.
+        
+    raise Exception("There was a problem and no suitable number was found for e.")
+
+
+def encrypt_or_decrypt(message: int, key: Key):
+    return pow(message, key.exponent) % key.modulus
+
 
 def main():
     public, private = create_keys(2, 7)
-
     print(f"n = {public.modulus}, e = {public.exponent}, d = {private.exponent}")
+
+    m = 2
+    c = encrypt_or_decrypt(m, public)
+    decrypted_message = encrypt_or_decrypt(c, private)
+
+    print(f"m = {m}, c = {c}, decrypted message = {decrypted_message}")
 
 
 if __name__ == "__main__":
